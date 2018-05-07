f525cd9e4cf67949c7b26c5bd9e5c04158a5bcc4
Hnatekmar <m.hnatma@gmail.com>
2018-05-07 17:51:02 +0200

    Application of physics system on car entity

--- src/simulation/components/physics.js
+++ src/simulation/components/physics.js
@@ -2,6 +2,7 @@ import * as CES from 'ces'
 import * as p2 from 'p2'
 
 export default CES.Component.extend({
+  name: 'physics',
   init: function (weight, pos, shapes) {
     this.body = new p2.Body({
       mass: weight,
--- src/simulation/main.js
+++ src/simulation/main.js
@@ -1,5 +1,6 @@
 
 import GraphicsSystem from './systems/graphics.js'
+import PhysicsSystem from './systems/physics.js'
 import Car from './entities/car.js'
 
 const CES = require('ces')
@@ -13,16 +14,20 @@ export default class Simulation {
     const graphics = new GraphicsSystem()
     graphics.setCanvas(canvasElement)
     this.world.addSystem(graphics)
-    this.world.addEntity(Car(150, 150))
-    requestAnimationFrame(() => this.update())
+    this.world.addSystem(new PhysicsSystem())
+    this.world.addEntity(Car(50.0, 0.0))
+    requestAnimationFrame((dt) => this.update(dt))
+    this.lastDt = 0
   }
 
   /**
    * Main simulation loop
    */
-  update () {
-    this.world.update()
-    requestAnimationFrame(() => this.update())
+  update (dt) {
+    const delta = dt - this.lastDt
+    this.lastDt = dt
+    this.world.update(delta / 1000.0)
+    requestAnimationFrame((dt) => this.update(dt))
   }
 
   /**
